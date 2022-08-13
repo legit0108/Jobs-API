571d91a9abc034c1aea0b5975e6bac7a442dcbda
legit0108 <legitgamer2345@gmail.com>
2022-08-13 15:38:08 +0530

    Implemented Compare Password

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,7 +18,13 @@ const login = async(req,res) => {
 
     if(!user) throw new UnauthenticatedError('Invalid Credentials')
 
-    const token = user.createJWT();
+    const isPasswordCorrect = await user.comparePassword(password)
+    
+    if(!isPasswordCorrect){
+        throw new UnauthenticatedError('Invalid Credentials')   
+    }
+
+    const token = user.createJWT()
     res.status(StatusCodes.OK).json({user:{name:user.name},token})
 }
 
