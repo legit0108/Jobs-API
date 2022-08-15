69ae0d277eeabc18d00c37b9ec7e451acbb7b841
legit0108 <legitgamer2345@gmail.com>
2022-08-15 16:00:38 +0530

    Initiated refactoring of Mongoose errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,21 @@
-const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || 'Something went wrong try again later'
   }
 
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({message: err.message })
+  // }
+
+  if(err.code && err.code===11000){
+    customError.message = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please chose another value`
+    customError.statusCode = 400
+  }
+
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(customError.statusCode).json({message: customError.message})
 }
 
 module.exports = errorHandlerMiddleware
