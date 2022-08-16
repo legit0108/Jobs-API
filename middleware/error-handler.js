db4c3e120ebb9a4c067bec3e526121ca51e851bc
legit0108 <legitgamer2345@gmail.com>
2022-08-16 20:04:00 +0530

    User friendly error messages and security options

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -8,10 +8,20 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   // if (err instanceof CustomAPIError) {
   //   return res.status(err.statusCode).json({message: err.message })
   // }
+  
+  if(err.name==='ValidationError'){
+    customError.message = Object.values(err.errors).map((item)=>item.message).join(',')
+    customError.statusCode = 400
+  }
 
   if(err.code && err.code===11000){
     customError.message = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please chose another value`
     customError.statusCode = 400
+  }
+
+  if(err.name === 'CastError'){
+    customError.message = `No item found with id : ${err.value}`
+    customError.statusCode = 404
   }
 
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
